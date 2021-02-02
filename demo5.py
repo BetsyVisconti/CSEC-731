ad4ee89394d4a207aab012026f329d8774404c0c
Betsy Visconti <betsyvisconti@gmail.com>
2021-02-02 18:02:10 -0500

    version of demo5

---
+++ demo5.py
@@ -0,0 +1,74 @@
+import socket
+import sys
+import ssl
+#import urllib
+data=""
+conn_type = sys.argv[1].split("://")[0]
+url = sys.argv[1].split("//")[1]
+host = url.split("/")[0]
+page = url[len(host):]
+if page == "":
+    page = "/"
+req = "GET " + page + " HTTP/1.1 \r\n"
+req += "Accept-Encoding: identity \r\n"
+req += "Host: " + host + "\r\n"
+req += "Connection: close \r\n\r\n"
+print(req)
+# set up IPv4 socket
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# if HTTP, make a connection, encode request, send over encoded request,
+# recieve the response and then pring the decoded response
+if conn_type == "http":
+    conn.connect((host, 80))
+    conn.send(req.encode())
+    print(conn.recv(8192).decode())
+    conn.close()
+# If HTTPS, we need set up SSLContext
+# wrapper for a socket declaring which version of SSL we want to use
+# use context to wrap socket and check host name in encryption certificate matches host name we access
+# next we connect using new s_conn object created within this elif statement and connect through port 443
+elif conn_type == "https":
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)  # wrapper for a socket
+    s_conn = context.wrap_socket(conn, server_hostname=host)  # check host name
+    s_conn.connect((host, 443))  # connect using new s_conn object made in previous line
+    s_conn.send(req.encode())  # encode data
+    data = s_conn.recv(8192).decode()  # decode request
+# if there's more than 8k of data, and data is not empty, print and grab more data
+# Ensures that if more than 8k data is sent, we recieve more than 8k data back
+    while data != "":
+        data = s_conn.recv(8192).decode()  # grab more data
+    conn.close()
+    list = []
+    data.replace("\n", " ")
+    data.replace("\r", " ")
+    data.replace("\t", " ")
+    for part in data.split(" "):
+        if ".js'" in part or '.js"' in part:
+            pos=1
+            while not pos == -1:
+                pos = part.find(".js")
+                if not pos == -1:
+                    for i in range(pos+2,0,-1):
+                        if part[i] == '"' or part[i]=="'":
+                            if not (part[i+1:pos+3] in list):
+                                list.append(part[i+1:pos+3])
+                    part = part[pos+3:]
+    for file in list:
+        print(file)
+    print("JS Files found: " + str(len(list)))
+
+    
+        
+#for part in resp.split(" "):
+#    if ".js'" in part or '.js"' in part:
+#        pos = 1
+#        while not pos == -1:
+#            pos = part.find(".js")
+#            if not pos == -1:
+#                for i in range(pos + 2, 0, -1):
+#                    if part[i] == '"' or part[i] == "'":
+#                        if not (part[i + 1:pos + 3] in list):
+#                            list.append(part[i + 1:pos + 3])
+#                part = part[pos + 3:]
+#for file in list:
+#    print(file)
